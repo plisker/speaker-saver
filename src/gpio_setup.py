cee91ba89748786e5cf00a0199fce1635a7190c3
Paul Lisker <plisker@gmail.com>
2024-08-26 20:21:24 -0500

    attempt to fix circular import

--- src/gpio_setup.py
+++ src/gpio_setup.py
@@ -3,20 +3,18 @@ from typing import Optional
 from src.controllers.smart_plug_controller import SmartPlugController
 
 
-GPIO_INSTALLED = False
-
-try:
-    import RPi.GPIO as GPIO  # type: ignore
-    from src.controllers.button_controller import ButtonController
-
-    GPIO_INSTALLED = True
-except ImportError as e:
-    logging.warn("Unable to import GPIO library. Error: ", e)
-
-
 def instantiate_button_controller(
     speakers_controller: SmartPlugController, mixer_controller: SmartPlugController
-) -> Optional["ButtonController"]:
+) -> Optional["ButtonController"]: # type: ignore
+    GPIO_INSTALLED = False
+    try:
+        import RPi.GPIO as GPIO  # type: ignore
+        from src.controllers.button_controller import ButtonController
+
+        GPIO_INSTALLED = True
+    except ImportError as e:
+        logging.warn("Unable to import GPIO library. Error: ", e)
+
     if GPIO_INSTALLED:
         logging.info("Setting up button controller.")
         return ButtonController(
